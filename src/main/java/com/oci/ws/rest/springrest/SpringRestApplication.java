a9d497a2e595819ec53f9e0aa3675adc0051f090
Karlos Chita <karloslcp@gmail.com>
2021-09-16 18:14:20 -0500

    Initial commit

---
+++ src/main/java/com/oci/ws/rest/springrest/SpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.oci.ws.rest.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApplication.class, args);
+    }
+
+}

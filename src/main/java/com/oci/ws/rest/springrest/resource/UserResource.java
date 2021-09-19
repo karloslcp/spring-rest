3c91123514420b38fea419280972329dc2f256fd
Karlos Chita <karloslcp@gmail.com>
2021-09-19 13:14:34 -0500

    Add create user endpoint

--- src/main/java/com/oci/ws/rest/springrest/resource/UserResource.java
+++ src/main/java/com/oci/ws/rest/springrest/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.oci.ws.rest.springrest.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -10,10 +11,14 @@ import com.oci.ws.rest.springrest.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("users")
@@ -35,5 +40,19 @@ public class UserResource
         User user = service.getUser(id);
 
         return EntityModel.of(user, linkToAllUsers);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@RequestBody User user)
+    {
+        Integer newUserId = service.createUser(user);
+
+        URI location = ServletUriComponentsBuilder
+            .fromCurrentRequestUri()
+            .path("/{id}")
+            .buildAndExpand(newUserId)
+            .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
